626c3332b779d745492856ed2cb0c9c475c06e3d
eunjeong-97 <eoss7777@naver.com>
2022-06-30 16:38:16 +0900

    3.6 middleware (2)
    - app.use()를 통해 global middleware만들거나
    - app.get('/', middleware, finalware)를 통해 한 path에서만 작동되게 하거나
    - controller에서 return하면 서버와의 통신을 종료하기 때문에 다음코드가 실행되지 않는다

--- src/server.js
+++ src/server.js
@@ -2,15 +2,17 @@ import express from 'express';
 const PORT = 4000;
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-    // request에 응답을 하려면 return을 해야하는데 
-    //return을 하게 되면 next()를 통해 다음 함수를 호출할 수 없기 때문에
-    // middleware는 request에 응답하지 않는다 단지 request를 지속시켜줄 뿐이다
-    // 여기서의 middleware는 유저가 우리 웹사이트의 어디를 가려는지 알려줄 것이다
-    console.log(`I'm middleware`);
+const logger = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url} by: ${req.method}`);
+    next();
+}
 
-    // request object연습
-    console.log(`Someone is going to: ${req.url}`);
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === '/protected') {
+        return res.send('<h1>Not Allowed</h1>'); // 반환하는순간 연결종료
+    }
+    console.log('Allow, you may pass.')
     next();
 }
 
@@ -18,25 +20,21 @@ const handleHome = (req, res) => {
     console.log(`I'm finalware`)
     return res.send('<h1>I still love you.</h1>');
 };
-const handleLogin = (req, res) => {
-    return res.send({ message: 'Login Here!' })
-};
-// get()메서드는 여러개의 controller를 가질 수있다
-// 그중 gossipMiddleware()는 middleware, handleware()함수는 finalware이다
-// 해당 middleware는 / URL에만 적용되어있는데 여러 path에서 사용하면 더 좋을것같다
-app.get('/', gossipMiddleware, handleHome);
-// express가 처음에는 gossipMiddleware()함수를 호출하는데
-// gossipMiddleware() 함수 내부에 있는 next()를 보고, 다음 한수인 handleHome()함수를 호출한다
-// 그래서 어떤거든 middleware가 될 수 있지만, 이 경우 handleHome()함수는 return하는것이 있기 때문에 맨 마지막으로 실행될 것이고 (=다음함수를 호출할 수 없음) 따라서 finalware이다
-// 모든 controller는 middleware가 될 수 있다
-// next함수가 있다면 middleware로 쓰이는걸 알 수 있다
-app.get('/login', handleLogin);
 
+// finalware이기 때문에 next인자나 함수호출 필요없음
+const handleProtected = (req, res) => {
+    return res.send('Welcome to the private lounge.')
+}
+
+// app.use();은 global middleware를 만들 수 있게 한다
+// 즉, 어느 URL에서도 작동된다
+// app.get()에서 middleware를 활용하기 전에 app.use()로 global middleware를 만들어야 한다(순서)
+// 자바스크립트는 위에서 아래방향으로 순서대로 처리하기 때문에 순서가 중요하다
+// 어느 path에 가던 gossipMiddleware() controller는 실행된다
+// 미들웨어 설치!: global하게 미들웨어를 둘 수도 있지만, 한 path에서만 작동되는 middleware을 만들 수도 잇음
+app.use(logger);
+app.use(privateMiddleware);
+app.get('/', handleHome);
+app.get('/protected', handleProtected);
 const handleListening = () => console.log(`Server Listening on http:localhost:${PORT}`);
 app.listen(PORT, handleListening);
-
-// middleware: 중간에 잇는 소프트웨어
-// 브라우저가 뭔가 request하고 서버가 다시 response하는 사이에 존재
-// 따라서 handleHome, handleLogin함수는 middleware이고, handler가 아니라 controller이다
-// 이러한 controller에는 request, response, next라는 argument가 있다
-// next는 다음 함수를 호출한다: next()함수를 호출하면, express가 handleHome의 다음의 함수를 호출한다
