555b6c4c49a314f468df94cbd6e8ad2974d05e92
eunjeong-97 <eoss7777@naver.com>
2022-06-30 15:24:10 +0900

    3.4 response 할 수있는종류: string 뿐만 아니라 html, status, json 등등 가능함

--- src/server.js
+++ src/server.js
@@ -2,16 +2,11 @@ import express from 'express';
 const PORT = 4000;
 const app = express();
 
-// '/' path로 GET요청을 하면 express는 handler인 handleHome()함수에게 두개의 object를 전달한다
-// request는 어떻게 요청했는지에 대한 정보를 알 수 있다
-// response
-const handleHome = (request, response) => {
-    // response를 return한다
-    // return response.end(); // request 종료시킴
-    return response.send('I still love you'); // 메시지전달
+const handleHome = (req, res) => {
+    return res.send('<h1>I still love you.</h1>');
 };
-const handleLogin = (res, req) => {
-    return req.send('Login Here!')
+const handleLogin = (req, res) => {
+    return res.send({ message: 'Login Here!' })
 }
 app.get('/', handleHome);
 app.get('/login', handleLogin);
@@ -19,6 +14,6 @@ app.get('/login', handleLogin);
 const handleListening = () => console.log(`Server Listening on http:localhost:${PORT}`);
 app.listen(PORT, handleListening);
 
-// handler에는 event가 있다
-// button.addEventListener('click', event => console.log(event.target.value))
-// express에선 route handler에는 event가 없지만 2개의 object가 있다
+// 공식문서를 보면 application 많은 속성이 있으니 필요할때마다 찾아보자
+// request object는 파일을 포함하거나, search query, username, password를 포함하는 경우도 있다
+// request에서 이러한데이터를 받아서 가공해야 한다
