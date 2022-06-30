04baa43c2bac7473570b13580ca26b5b59fb1045
eunjeong-97 <eoss7777@naver.com>
2022-06-30 17:11:28 +0900

    3.7 setup recap

--- src/server.js
+++ src/server.js
@@ -21,20 +21,37 @@ const handleHome = (req, res) => {
     return res.send('<h1>I still love you.</h1>');
 };
 
-// finalware이기 때문에 next인자나 함수호출 필요없음
 const handleProtected = (req, res) => {
     return res.send('Welcome to the private lounge.')
 }
 
-// app.use();은 global middleware를 만들 수 있게 한다
-// 즉, 어느 URL에서도 작동된다
-// app.get()에서 middleware를 활용하기 전에 app.use()로 global middleware를 만들어야 한다(순서)
-// 자바스크립트는 위에서 아래방향으로 순서대로 처리하기 때문에 순서가 중요하다
-// 어느 path에 가던 gossipMiddleware() controller는 실행된다
-// 미들웨어 설치!: global하게 미들웨어를 둘 수도 있지만, 한 path에서만 작동되는 middleware을 만들 수도 잇음
 app.use(logger);
 app.use(privateMiddleware);
 app.get('/', handleHome);
 app.get('/protected', handleProtected);
 const handleListening = () => console.log(`Server Listening on http:localhost:${PORT}`);
 app.listen(PORT, handleListening);
+
+/*
+package.json: node에 관한 정보를 저장하는 파일이다
+
+scripts
+별명을 저장하고 npm run (script name) 명령어를 입력하면 실행가능
+어떤 경우에는 scriptrs를 순서대로 호출할 수도 있다 (나중에)
+
+dependencies는 프로젝트가 돌아가기 위해 필요한 package들 모아둠
+express에 대한 정보는 패키지를 설치하면서 자동으로 저장됨(버전)
+이러한 패키지들은 node_modules폴더에 저장되기 때문에 node_modules폴더를 전달할 필요가 없다
+이렇게 node_modules를 업로드 하지 않기 위해 .gitignore에 node_modules/ 추가
+package.json파일만 잇으면 다시 node_modules폴더에 패키지 설치가능
+
+dependencies vs devDependencies: 체계화하기 위해 나눔
+dependencies: 프로젝트가 실행되기 위해 꼭 필요한 패키지
+devDependencies: 프로젝트가 실행될때 무조건 필요한건 아니지만 개발자가 개발을 할 때 필요한것, 개발환경을 더 편하게
+
+babel: 모던자바스크립트코드를 이해하지 못하는경우도 있는데
+그때문에 node로 실행하는것이 아니라 babel-node로 프로젝트를 실행한다
+이러한 컴파일과정을 위한 설정은 babel.config.json에서 우리가 babel에 추가하고 싶은 plugin을 넣으면 되는데, react.js를 사용하면 react.js preset을 사용할 수 있다
+
+파일이 변경될때마다 서버를 재시작해주는 nodemon
+*/
