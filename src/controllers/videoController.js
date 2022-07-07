25e5f006b4dec132c0b3dcd6a0e03078e8e1223a
eunjeong-97 <eoss7777@naver.com>
2022-07-07 12:53:20 +0900

    4.7 URL parameter(1): 노마드코더강의같이 동일한 작업을 하면서 id값만 다른 경우 URL에 변수를 넣을 수 있는 paramter를 활용하면 좋다, 만약 이게 없으면 /1 /2 /3같은 router를 각각 만들어줘야 한다

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,9 @@
 // 이런식으로 여러개로 export를 할 수도 잇다
 export const trending = (req, res) => res.send('Home Page Videos');
 export const upload = (req, res) => res.send('Upload Video');
-export const see = (req, res) => res.send('See Video');
+export const see = (req, res) => {
+    console.log(req.params); // ID 변수명(우리가 지정한 파라미터명)과 값을 확인할 수잇다 {id:12121212}
+    res.send(`See Video: ${req.params.id}`);
+}
 export const edit = (req, res) => res.send('Edit Video');
 export const remove = (req, res) => res.send('Remove Video');
--- src/routes/videoRouter.js
+++ src/routes/videoRouter.js
@@ -3,9 +3,19 @@ import { remove, edit, see, upload } from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
-videoRouter.get('/upload', upload);
+videoRouter.get('/upload', upload); // 그래서 변수가없는 path는 제일 상단에 둬야 한다
 videoRouter.get('/:id', see);
+videoRouter.get('/upload', upload);
+// 만약 /upload path를 여기로 위치시키면, express는 upload path를 id값으로 인식해서 see함수를 실행시킨다 {id:'upload'}
+// videoRouter.get('/1', see);
+// videoRouter.get('/2', see);
 videoRouter.get('/:id/edit', edit);
 videoRouter.get('/:id/delete', remove);
 
 export default videoRouter;
+
+/*
+URL에 :paramter가 있으면 URL에 변수를 넣을 수있게 해준다
+만약 여기에 잇는 변수가 없다면 매 강의영상마다 매번 라우터를 만들어줘야 한다
+그래서 URL에 파라미터인 변수를 넣어주는거다 
+*/
