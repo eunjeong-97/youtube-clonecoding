2a5530f27211d1b0b1e360c1eab54d0d89cbafef
eunjeong-97 <eoss7777@naver.com>
2022-07-07 11:41:52 +0900

    4.5 Architecture Recap: controller의 모듈화, 보기좋은 URL을 위해 globalRouter는 있지만 globalController는 없다

--- src/server.js
+++ src/server.js
@@ -18,9 +18,17 @@ const handleListening = () => console.log(`Server Listening on http:localhost:${
 app.listen(PORT, handleListening);
 
 /*
-server.js에서 app.get()으로 각각의 URL별로 controller를 관리하면 너무 복잡해져서
-URL이 어떻게 시작하는지에 따라 나누는 방법인 라우터를 이용해서 그룹으로 관리하기로 했다
+효율적으로 관리하기 위해 routes폴더와 controllers폴더를 만들었는데
+server.js에서나 router파일에서 app.use()를 할때 다른파일에서 import해서 사용하는데
+이렇게 import하기 위해서는 해당파일에서 먼저 export를 해야한다
 
-그리고 도메인을 기반해서 그룹을 골랐는데 
-URL을 더 낫고 독립적인 방법으로 관리하기 위해 라우터를 사용한다
+단순히 const로 정의한것만으로는 공유가 되지 않기 때문에 
+이 파일의 어떤걸 다른파일과 공유하고 싶다면 무엇을 공유하고 싶은지 지정해야 한다
+해당 파일에서 공유하고 싶은게 하나라면 export default하면 되고 다른파일에서 import를 하면 공유할 대상 그대로 전해진다
+이름은 상관없이 rename해도 됨: 어떻게 어떤이름으로 import하도 상관없음
+
+한편, 각각의 const로 정의한거에 export를 하면 object를 열어야 하고, 정의한 이름 그대로 가져와서 사용해야 한다
+
+글로벌라우터에서는 videoController와 userController에서 함수를 가져와서 사용한다
+글러벌라우터를 사용하는 이유는 URL을 보기 좋게 하기 위해
 */
